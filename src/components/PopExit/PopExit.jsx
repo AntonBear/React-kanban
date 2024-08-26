1850086fe6a4a831a52f0080dbd79efb76175a69
Anton <holzed15@gmail.com>
2024-08-26 17:13:33 +0300

    feat: add <link> to PopExit

--- src/components/PopExit/PopExit.jsx
+++ src/components/PopExit/PopExit.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function PopExit() {
   return (
@@ -11,10 +12,12 @@ function PopExit() {
           <form className="pop-exit__form" id="formExit" action="#">
             <div className="pop-exit__form-group">
               <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                <a href="modal/signin.html">Да, выйти</a>{' '}
+                <Link to="/">Да, выйти</Link>
+                {/* <a href="modal/signin.html">Да, выйти</a>{' '} */}
               </button>
               <button className="pop-exit__exit-no _hover03" id="exitNo">
-                <a href="main.html">Нет, остаться</a>{' '}
+                <Link to="/">Нет, остаться</Link>
+                {/* <a href="main.html">Нет, остаться</a>{' '} */}
               </button>
             </div>
           </form>
